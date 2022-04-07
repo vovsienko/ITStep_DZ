d5e603c1b1d67ce315f207a788624702433ca864
Viktoriia Ovsiienko <v.ovs76@gmail.com>
2022-04-07 00:02:16 -0500

    Add files via upload

---
+++ vb.net-lessons/DialogForm/Form1 (2021_02_07 03_21_04 UTC).vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub btnNewCaption_Click(sender As Object, e As EventArgs) Handles btnNewCaption.Click
+        Dim frmCaption As New Form2
+        frmCaption.ShowDialog(Me)
+        If frmCaption.DialogResult = Windows.Forms.DialogResult.OK Then
+            Me.Text = frmCaption.txtNewCaption.Text
+        End If
+
+        'comparison, if the form had three buttons - Yes, No, Cancel
+        '       If frmCaption.DialogResult =
+        'Windows.Forms.DialogResult.Yes Then
+        '           ' execute code based on user clicking Yes button
+        '       ElseIf frmCaption.DialogResult =
+        '        Windows.Forms.DialogResult.No Then
+        '           ' execute code based on user clicking No button
+        '       Else
+        '           ' execute code based on user clicking Cancel button
+        '       End If
+
+        'to prevent from closing
+        'Me.DialogResult = Windows.Forms.DialogResult.None
+    End Sub
+End Class
---
+++ vb.net-lessons/DialogForm/Form2 (2021_02_07 03_21_04 UTC).vb
@@ -0,0 +1,22 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim frm As Form1
+        frm = CType(Me.Owner, Form1)
+        txtNewCaption.Text = frm.Text
+    End Sub
+
+    Private Sub btnOK_Click(ByVal sender As Object,
+ByVal e As System.EventArgs) Handles btnOK.Click
+        'Dim str As String
+        'str = txtNewCaption.Text.Trim
+        'If str.Length = 0 Then
+
+        'combined version of the above 3 lines:
+        If txtNewCaption.Text.Trim.Length = 0 Then
+
+            MessageBox.Show("Type text or click Cancel")
+            Me.DialogResult = Windows.Forms.DialogResult.None
+            txtNewCaption.Focus()
+        End If
+    End Sub
+End Class
