50a91ad91b9b76831f03bf20db7a70987cb7026a
Viktoriia Ovsiienko <v.ovs76@gmail.com>
2022-04-07 00:17:29 -0500

    move

---
+++ vb.net-lessons/WinFormsApp3_change_color_btns/WinFormsApp3_chng_color_btn/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnRed = New System.Windows.Forms.Button()
+        Me.btnGreen = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.BackColor = System.Drawing.Color.White
+        Me.lblDisplay.Location = New System.Drawing.Point(263, 91)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(120, 40)
+        Me.lblDisplay.TabIndex = 0
+        '
+        'btnRed
+        '
+        Me.btnRed.Location = New System.Drawing.Point(146, 199)
+        Me.btnRed.Name = "btnRed"
+        Me.btnRed.Size = New System.Drawing.Size(120, 40)
+        Me.btnRed.TabIndex = 1
+        Me.btnRed.Text = "Red"
+        Me.btnRed.UseVisualStyleBackColor = True
+        '
+        'btnGreen
+        '
+        Me.btnGreen.Location = New System.Drawing.Point(372, 199)
+        Me.btnGreen.Name = "btnGreen"
+        Me.btnGreen.Size = New System.Drawing.Size(120, 40)
+        Me.btnGreen.TabIndex = 2
+        Me.btnGreen.Text = "Green"
+        Me.btnGreen.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnGreen)
+        Me.Controls.Add(Me.btnRed)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents btnRed As Button
+    Friend WithEvents btnGreen As Button
+End Class
---
+++ vb.net-lessons/WinFormsApp3_change_color_btns/WinFormsApp3_chng_color_btns-master/WinFormsApp3_chng_color_btn/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnRed_Click(sender As Object, e As EventArgs) Handles btnRed.Click
+        lblDisplay.BackColor = Color.Red
+    End Sub
+
+    Private Sub btnGreen_Click(sender As Object, e As EventArgs) Handles btnGreen.Click
+        lblDisplay.BackColor = Color.Green
+    End Sub
+End Class
