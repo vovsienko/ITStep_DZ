faed13a4d7e4a536907f530c84c7740f2318bf60
Viktoriia Ovsiienko <v.ovs76@gmail.com>
2022-04-06 22:52:33 -0500

    edit all

--- WinFormsApp3_chng_color_btns/WinFormsApp3_chng_color_btns-master/WinFormsApp3_chng_color_btn/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub btnRed_Click(sender As Object, e As EventArgs) Handles btnRed.Click
-        lblDisplay.BackColor = Color.Red
-    End Sub
-
-    Private Sub btnGreen_Click(sender As Object, e As EventArgs) Handles btnGreen.Click
-        lblDisplay.BackColor = Color.Green
-    End Sub
-End Class
