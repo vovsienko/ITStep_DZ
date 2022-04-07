76698a58aaff3ec741a0f0023cfcd57a36298eff
Viktoriia Ovsiienko <v.ovs76@gmail.com>
2022-04-06 23:36:02 -0500

    move

--- vb.net-lessons/WinFormsApp1/WinFormsApp1/Form2 (2021_02_07 03_21_04 UTC).vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Form2
-    Private Sub Form2_DoubleClick(sender As Object, e As EventArgs) Handles Me.DoubleClick
-        Me.Text = "Double Clicked"
-        Me.BackColor = Color.Azure
-    End Sub
-End Class
